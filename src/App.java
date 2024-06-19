585fd64932f86f1da0e2e05d0f5a251e5fbcf633
Arnaud G <girardarnaud111@gmail.com>
2024-06-19 15:34:38 -0400

    add normal double dabble

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // 7.5 ms
-        BigNumber number = new BigNumber("999999999999999999999999999999999999999999999999999999999");
-
-        for (String string : number.toBinary()) {
-            System.out.print(string);
-        }
+        BigNumber number = new BigNumber("14294967296");
     }
 }
--- src/BigNumber.java
+++ src/BigNumber.java
@@ -8,6 +8,10 @@ public class BigNumber {
     public BigNumber(String number) {
         this.BCD = numberToBCD(number);
         this.binary = doubleDabble(this.BCD);
+
+        for (byte b : doubleDabble(this.binary)) {
+            System.out.println(b);
+        }
     }
 
     public BigNumber(BigNumber number) {
@@ -27,7 +31,68 @@ public class BigNumber {
         for (int i = 0; i < number.length(); i++) {
             BCD[i] = Byte.valueOf(String.valueOf(number.charAt(i)));
         }
+
         return BCD;
+    }
+
+    private byte[] doubleDabble(long[] binary) {
+        byte[] BCD = new byte[((binary.length * 64) - 4) / 2 + 1]; // Maximum possible size
+        int binaryArrayIndex = binary.length - 1;
+        int bitPosition = 63;
+        boolean isComplete = false;
+        boolean hasOverflown = false;
+
+        while(!isComplete) {
+            if((binary[binaryArrayIndex] & (1L << bitPosition)) == (1L << bitPosition)) {
+                hasOverflown = true;
+            }
+
+            for (int i = 0; i < BCD.length; i++) {
+                boolean isOverflowing = hasOverflown;
+                hasOverflown = false;
+
+                if(BCD[i] >= 5) {
+                    BCD[i] += 3;
+                }
+
+                if((BCD[i] & 8) == 8) {
+                    hasOverflown = true;
+                }
+
+                BCD[i] <<= 1;
+
+                BCD[i] &= 0b00001111; // remove any bits that exceed the limit
+
+                if(isOverflowing) {
+                    BCD[i] |= 1;
+                }
+            }
+            
+            bitPosition--;
+
+            if(bitPosition == -1) {
+                binaryArrayIndex--;
+                bitPosition = 63;
+            }
+
+            hasOverflown = false;
+
+            if(binaryArrayIndex == -1 && bitPosition == 63) {
+                isComplete = true;
+            }
+        }
+
+        int numberOfTrimsNeeded = 0;
+        for (int i = BCD.length - 1; i >= 0; i--) {
+            if(BCD[i] == 0) {
+                numberOfTrimsNeeded++;
+            } else {
+                break;
+            }
+        }
+
+        // Trim the array to the actual size
+        return Arrays.copyOf(BCD, BCD.length - numberOfTrimsNeeded);
     }
 
     private long[] doubleDabble(byte[] BCD) {
@@ -82,6 +147,7 @@ public class BigNumber {
     @Override
     public String toString() {
         String number = "";
+        byte[] BCD = doubleDabble(this.binary);
 
         for (int i = 0; i < BCD.length; i++) {
             number = String.valueOf(BCD[i]) + number;
