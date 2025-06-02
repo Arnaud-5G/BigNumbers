32327eba8172c518bfa1194e7fbfddc9a456a9f8
Arnaud G <girardarnaud111@gmail.com>
2025-06-02 16:42:09 -0400

    add addOne/subOne/add + lessThan/GreaterThan

--- src/App.java
+++ src/App.java
@@ -1,5 +1,87 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BigNumber number = new BigNumber("14294967296");
+        System.out.println("Testing addOne:");
+        double startTime = System.nanoTime();
+        BigNumber number = new BigNumber("1000000000000000000000000");
+        for (int i = 0; i < 370000000; i++) {
+            number.addOne();
+        }
+        double elapsedTime = (System.nanoTime() - startTime) / 1000000d; // Convert to milliseconds
+
+        System.out.println("Time taken: " + elapsedTime + " ms"); // 105.8887 ms
+        System.out.println("Average time taken: " + (elapsedTime / 370000000d) * 1000000d + " nano"); // 0.28618567567567565 nano
+
+        System.out.println("Testing add with long:");
+        startTime = System.nanoTime();
+        number = new BigNumber("1000000000000000000000000");
+        number.add(370000000);
+        elapsedTime = (System.nanoTime() - startTime) / 1000000d; // Convert to milliseconds
+
+        System.out.println("Time taken: " + elapsedTime + " ms"); // 164.9893 ms
+        System.out.println("Average time taken: " + (elapsedTime / 370000000d) * 1000000d + " nano"); // 0.445917027027027 nano
+
+        System.out.println("Testing add with BigNumber:");
+        startTime = System.nanoTime();
+        number = new BigNumber("1000000000000000000000000");
+        BigNumber number1 = new BigNumber("370000000");
+        number.add(number1);
+        elapsedTime = (System.nanoTime() - startTime) / 1000000d; // Convert to milliseconds
+
+        System.out.println("Time taken: " + elapsedTime + " ms"); // 946.792 ms
+        System.out.println("Average time taken: " + (elapsedTime / 370000000d) * 1000000d + " nano"); // 2.5588972972972974 nano
+
+        // testAddOne();
+        // testSubOne();
+        // testAdd();
+    }
+
+    public static void testAddOne() {
+        BigNumber number = new BigNumber("1000000000000000000000000");
+        BigNumber number1 = new BigNumber("9999999999");
+        BigNumber number2 = new BigNumber("-1000000000000000000000000");
+        BigNumber number3 = new BigNumber("-10000000000000000000000000000000000000");
+        BigNumber number4 = new BigNumber("0");
+        BigNumber number5 = new BigNumber("-1");
+        BigNumber number6 = new BigNumber("18446744073709551617"); // 2^64 + 1
+        BigNumber number7 = new BigNumber("18446744073709551616"); // 2^64
+        BigNumber number8 = new BigNumber("18446744073709551615"); // 2^64 - 1
+        System.out.println(number.addOne().toString()); // 1000000000000000000000001
+        System.out.println(number1.addOne().toString()); // 10000000000
+        System.out.println(number2.addOne().toString()); // -999999999999999999999999
+        System.out.println(number3.addOne().toString()); // -9999999999999999999999999999999999999
+        System.out.println(number4.addOne().toString()); // 1
+        System.out.println(number5.addOne().toString()); // 0
+        System.out.println(number5.addOne().toString()); // 1
+        System.out.println(number6.addOne().toString()); // 18446744073709551618
+        System.out.println(number7.addOne().toString()); // 18446744073709551617
+        System.out.println(number8.addOne().toString()); // 18446744073709551616
+    }
+
+    public static void testSubOne() {
+        BigNumber number = new BigNumber("1000000000000000000000000");
+        BigNumber number1 = new BigNumber("9999999999");
+        BigNumber number2 = new BigNumber("-1000000000000000000000000");
+        BigNumber number3 = new BigNumber("-10000000000000000000000000000000000000");
+        BigNumber number4 = new BigNumber("0");
+        BigNumber number5 = new BigNumber("-1");
+        BigNumber number6 = new BigNumber("18446744073709551617"); // 2^64 + 1
+        BigNumber number7 = new BigNumber("18446744073709551616"); // 2^64
+        BigNumber number8 = new BigNumber("18446744073709551615"); // 2^64 - 1
+        System.out.println(number.subOne().toString()); // 999999999999999999999999
+        System.out.println(number1.subOne().toString()); // 9999999998
+        System.out.println(number2.subOne().toString()); // -1000000000000000000000001
+        System.out.println(number3.subOne().toString()); // -10000000000000000000000000000000000001
+        System.out.println(number4.subOne().toString()); // -1
+        System.out.println(number5.subOne().toString()); // -2
+        System.out.println(number6.subOne().toString()); // 18446744073709551616
+        System.out.println(number7.subOne().toString()); // 18446744073709551615
+        System.out.println(number8.subOne().toString()); // 18446744073709551614
+    }
+
+    public static void testAdd() {
+        BigNumber number = new BigNumber("1000000000000000000000000");
+        BigNumber number1 = new BigNumber("999999999");
+
+        System.out.println(number1.add(number).toString()); // 100000000000000000009999999999
     }
 }
--- src/BigNumber.java
+++ src/BigNumber.java
@@ -9,9 +9,15 @@ public class BigNumber {
         this.BCD = numberToBCD(number);
         this.binary = doubleDabble(this.BCD);
 
-        for (byte b : doubleDabble(this.binary)) {
-            System.out.println(b);
-        }
+        // for (byte b : doubleDabble(this.binary)) {
+        //     System.out.println(b);
+        // }
+    }
+
+    public BigNumber(long number) {
+        this.BCD = numberToBCD(Long.toString(number));
+        this.binary = doubleDabble(this.BCD);
+        this.negative = number < 0;
     }
 
     public BigNumber(BigNumber number) {
@@ -157,6 +163,10 @@ public class BigNumber {
             return "-" + number;
         }
 
+        if (number.equals("")) {
+            return "0";
+        }
+
         return number;
     }
 
@@ -181,7 +191,245 @@ public class BigNumber {
         return binary;
     }
 
+    //!             getters
+
     public boolean isNegative() {
         return this.negative;
+    }
+
+    public boolean isZero() {
+        for (long l : this.binary) {
+            if (l != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+    //!             comparison operations
+
+    public boolean equals(BigNumber number) {
+        if (this.isZero() && number.isZero()) {
+            return true;
+        }
+
+        if (this.isNegative() != number.isNegative()) {
+            return false;
+        }
+
+        if (this.binary.length != number.binary.length) {
+            return false;
+        }
+
+        for (int i = 0; i < this.binary.length; i++) {
+            if (this.binary[i] != number.binary[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public boolean isLessThan(BigNumber number) {
+        if (this.isZero() && number.isZero()) {
+            return false;
+        }
+
+        if (this.isNegative() && !number.isNegative()) {
+            return true;
+        }
+
+        if (!this.isNegative() && number.isNegative()) {
+            return false;
+        }
+
+        if (this.binary.length < number.binary.length) {
+            return !this.isNegative();
+        } else if (this.binary.length > number.binary.length) {
+            return this.isNegative();
+        }
+
+        for (int i = this.binary.length - 1; i >= 0; i--) {
+            if (this.binary[i] < number.binary[i]) {
+                return !this.isNegative();
+            } else if (this.binary[i] > number.binary[i]) {
+                return this.isNegative();
+            }
+        }
+
+        return false;
+    }
+
+
+    public boolean isGreaterThan(BigNumber number) {
+        if (this.isZero() && number.isZero()) {
+            return false;
+        }
+
+        if (this.isNegative() && !number.isNegative()) {
+            return false;
+        }
+
+        if (!this.isNegative() && number.isNegative()) {
+            return true;
+        }
+
+        if (this.binary.length > number.binary.length) {
+            return !this.isNegative();
+        } else if (this.binary.length < number.binary.length) {
+            return this.isNegative();
+        }
+
+        for (int i = this.binary.length - 1; i >= 0; i--) {
+            if (this.binary[i] > number.binary[i]) {
+                return !this.isNegative();
+            } else if (this.binary[i] < number.binary[i]) {
+                return this.isNegative();
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+    //!             operations
+
+    public BigNumber addOne() {
+        for(int i = 0; i < this.binary.length; i++) {
+            if(!negative) { // positive
+                if (binary[i] != ~0L) {
+                    binary[i]++;
+                    return this;
+                }
+    
+                binary[i] = 0;
+                if (i == this.binary.length - 1) { // add a new index if we are at the end
+                    binary = Arrays.copyOf(binary, binary.length + 1);
+                    binary[i+1] = 1;
+                    return this;
+                }
+            } else { // negative
+                if (binary[i] != 0) { // check if number is 1 if yes then == 0 -> positive
+                    if (this.binary.length == 1 && binary[0] == 1) {
+                        negative = false;
+                    }
+                    binary[i]--;
+
+                    if (i == this.binary.length - 1 && binary[i] == 0) { // if we are at the end and the last index is 0 then remove it
+                        binary = Arrays.copyOf(binary, binary.length - 1);
+                    }
+                    return this;
+                }
+
+                binary[i] = ~0L;
+            }
+        }
+
+        if (negative && binary[binary.length - 1] == ~0L) { // if all binaries = 0 then become 1
+            for (int i = 1; i < binary.length; i++) {
+                binary[i] = 0;
+            }
+
+            binary[0] = 1;
+            binary = Arrays.copyOf(binary, 1); // keep only the first index
+
+            negative = false;
+        }
+
+        return this;
+    }
+
+
+    public BigNumber subOne() {
+        for(int i = 0; i < this.binary.length; i++) {
+            if(!negative) { // positive
+                if (binary[i] != 0) {
+                    binary[i]--;
+                    if (i == this.binary.length - 1 && binary[i] == 0) { // if we are at the end and the last index is 0 then remove it
+                        binary = Arrays.copyOf(binary, binary.length - 1);
+                    }
+                    return this;
+                }
+
+                binary[i] = ~0L;
+            } else { // negative
+                if (binary[i] != ~0L) { // check if number is 0 if yes then == 1 -> positive
+                    if (this.binary.length == 1 && binary[0] == 0) {
+                        negative = false;
+                    }
+                    binary[i]++;
+                    return this;
+                }
+
+                binary[i] = 0;
+            }
+        }
+
+        if (!negative && binary[binary.length - 1] == ~0L) { // if all binaries = 0 then become 1
+            for (int i = 1; i < binary.length; i++) {
+                binary[i] = 0;
+            }
+
+            binary[0] = 1;
+            binary = Arrays.copyOf(binary, 1); // keep only the first index
+
+            negative = true;
+        }
+
+        return this;
+    }
+
+
+
+
+    public BigNumber add(BigNumber additive) { //TODO: optimize
+        if (additive.isZero()) {
+            return this;
+        }
+
+        if (this.isZero()) {
+            this.binary = additive.binary;
+            this.BCD = additive.BCD;
+            this.negative = additive.isNegative();
+            return this;
+        }
+
+        BigNumber smallest = this.isLessThan(additive) ? this : additive;
+
+        BigNumber largest = this.isLessThan(additive) ? additive : this;
+
+        while (!smallest.isZero()) {
+            largest.addOne();
+            if (smallest.isNegative()) {
+                smallest.addOne();
+            } else {
+                smallest.subOne();
+            }
+        }
+
+        return largest;
+    }
+
+
+    public BigNumber add(long additive) { //TODO: optimize
+        while (additive != 0) {
+            if (additive > 0) {
+                this.addOne();
+                additive--;
+            } else {
+                this.addOne(); // should be subtractOne()
+                additive++;
+            }
+        }
+
+        return this;
     }
 }
